f03ba0905580b3641433a974987c891494dfca05
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-08-09 22:08:42 -0500

    1.1: Course Information, step 1

---
+++ Part-1/introdemo/src/App.jsx
@@ -0,0 +1,77 @@
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+// const App = () => {
+//   const name = "Peter";
+//   const age = 10;
+//   return (
+//     <>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </>
+//   );
+// };
+
+const App = () => {
+  const friends = [
+    { name: "Peter", age: 4 },
+    { name: "Maya", age: 10 },
+  ];
+
+  const arr = ["Peter", "Maya"];
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name={friends[0].name} age={friends[0].age} />
+      <Hello name={friends[1].name} age={friends[1].age} />
+      <p>{arr}</p>
+    </>
+  );
+};
+
+export default App;
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
