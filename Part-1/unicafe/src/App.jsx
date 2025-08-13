7603518b60d3e3120bb9c3a54d4cd79613147326
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-08-12 20:12:06 -0500

    1.7: unicafe step 2

--- Part-1/unicafe/src/App.jsx
+++ Part-1/unicafe/src/App.jsx
@@ -15,22 +15,38 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [count, setCount] = useState([]);
 
   const handleGood = () => {
+    const goodScore = 1;
     const goodUpdated = good + 1;
     setGood(goodUpdated);
+    setCount(count.concat(goodScore));
   };
 
   const handleNeutral = () => {
+    const neutralScore = 0;
     const updatedNeutral = neutral + 1;
     setNeutral(updatedNeutral);
+    setCount(neutralScore);
+    setCount(count.concat(neutralScore));
   };
 
   const handleBad = () => {
+    const badScore = -1;
     const updatedBad = bad + 1;
     setBad(updatedBad);
+    setCount(badScore);
+    setCount(count.concat(badScore));
   };
 
+  const handleAverage = () => {
+    const sum = count.reduce((acc, total) => acc + total, 0);
+    const arrLength = count.length;
+    return sum / arrLength;
+  };
+
+  console.log(count);
   return (
     <>
       <h1>Give Feedback</h1>
@@ -41,6 +57,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {good + neutral + bad}</p>
+      <p>Average: {handleAverage() || 0}</p>
+      <p>Positive: {(good / (good + neutral + bad)) * 100 || 0}%</p>
     </>
   );
 };
