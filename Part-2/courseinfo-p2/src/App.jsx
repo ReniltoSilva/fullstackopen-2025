905733c88e382e5f4fa92348385c49d116d1746c
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-08-14 11:32:56 -0500

    2.4: Course information step 9

--- Part-2/courseinfo-p2/src/App.jsx
+++ Part-2/courseinfo-p2/src/App.jsx
@@ -1,65 +1,95 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
-  return <Course course={course} />;
-};
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-const Course = (props) => {
   return (
     <>
-      <Header header={props.course.name} />
-      <Content parts={props.course.parts} />
+      <h1>Web Development Curriculum</h1>
+      {courses.map((item) => (
+        <Course key={item.id} course={item} />
+      ))}
     </>
   );
 };
 
-const Header = ({ header }) => {
-  return <h1>{header}</h1>;
+const Course = ({ course }) => {
+  // console.log(course);
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={course.parts
+          .map((item) => item.exercises)
+          .reduce((a, b) => a + b)}
+      />
+    </>
+  );
+};
+
+const Header = ({ name }) => {
+  // console.log(name);
+  return <h2>{name}</h2>;
 };
 
 const Content = ({ parts }) => {
-  // console.log(parts);
-  //Reduce already calculated
-  const total = parts.map((item) => item.exercises).reduce((a, b) => a + b);
-
+  // console.log("Content:", parts);
   return (
     <>
       {parts.map((item) => (
-        <Part name={item.name} exercises={item.exercises} key={item.id} />
+        <Part key={item.id} name={item.name} exercises={item.exercises} />
       ))}
-      {<Total total={total} />}
     </>
   );
 };
 
-const Part = (props) => {
+const Part = ({ name, exercises }) => {
+  // console.log(name, exercises);
   return (
     <p>
-      {props.name} {props.exercises}
+      {name} {exercises}
     </p>
   );
 };
