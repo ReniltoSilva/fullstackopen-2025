f95a9fb444852ccd71c9b8966a377af9f0480c1f
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-09-15 20:30:59 -0500

    3.20*: Phonebook database, step 8

--- Part-3/phonebook-backend/models/person.js
+++ Part-3/phonebook-backend/models/person.js
@@ -22,7 +22,17 @@ const personSchema = new mongoose.Schema({
     ],
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: (props) => `${props.value} it's not a valid phone number`,
+    },
+    required: [true, "User phone number required"],
+  },
 
   // name: String,
   // number: String,
