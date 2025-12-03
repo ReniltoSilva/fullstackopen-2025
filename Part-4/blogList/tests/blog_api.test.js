efaa92a07576103a51083fbedea1134bac757c80
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-12-03 22:07:22 +0700

    4.9: Blog List Tests, step 2

--- Part-4/blogList/tests/blog_api.test.js
+++ Part-4/blogList/tests/blog_api.test.js
@@ -14,13 +14,21 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 });
 
-test.only("blogs are returned as json and correct amount", async () => {
+test("blogs are returned as json and correct amount", async () => {
   const response = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
 
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
+});
+
+test.only("verifies unique id property of the blog posts is named id", async () => {
+  const response = await api.get("/api/blogs");
+
+  const checkId = response.body[0];
+  assert.ok(checkId.id);
+  assert.strictEqual(checkId._id, undefined);
 });
 
 after(async () => {
