9d3042b0de86f1532c67740b651f8cbbc76fd89f
Renilto <juninho.dg@hotmail.com>
2026-01-30 18:01:59 +0700

    5.14: Blog List Tests, step 2

--- Part-5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part-5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,10 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { getByText, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { beforeEach, expect, vi } from "vitest";
 
 import Blog from "./Blog";
 
-test("renders blog's title and author", () => {
+describe("<Blog/>", () => {
+  beforeEach(() => {
+    render(<Blog blog={blog} />);
+  });
+
   const blog = {
     title: "Hello new World",
     id: "2341234sdf",
@@ -13,15 +17,29 @@ test("renders blog's title and author", () => {
     likes: 4,
   };
 
-  render(<Blog blog={blog} />);
+  test("renders blog's title and author but NOT url and likes", () => {
+    //Renders blog and title
+    const title = screen.getByText("Title: Hello new World");
+    const author = screen.getByText("Author: Junior Santos");
+    expect(title).toBeVisible();
+    expect(author).toBeVisible();
 
-  const title = screen.getByText("Title: Hello new World");
-  const author = screen.getByText("Author: Junior Santos");
-  const url = screen.queryByText("Url: www.com");
-  const likes = screen.queryByText("Likes: 4");
+    //Don't renders url and likes
+    const url = screen.queryByText("Url: www.com");
+    const likes = screen.queryByText("Likes: 4");
+    expect(url).not.toBeVisible();
+    expect(likes).not.toBeVisible();
+  });
 
-  expect(title).toBeVisible();
-  expect(author).toBeVisible();
-  expect(url).not.toBeVisible();
-  expect(likes).not.toBeVisible();
+  test("Show url and likes when 'view' button is clicked", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("View");
+    await user.click(button);
+
+    const url = screen.getByText("Url: www.com");
+    expect(url).toBeVisible();
+
+    const likes = screen.getByText("Url: www.com");
+    expect(likes).toBeVisible();
+  });
 });
