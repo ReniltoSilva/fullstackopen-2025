6dd885c5906ede151c95d1b6907a8c3f0d01b229
Renilto <juninho.dg@hotmail.com>
2026-02-05 22:34:00 +0700

    5.20: Blog List End To End Testing, step 4

--- E2E-tests-Playwright/tests/blog_app.spec.js
+++ E2E-tests-Playwright/tests/blog_app.spec.js
@@ -45,26 +45,30 @@ describe("Blog app", () => {
     // });
 
     describe("When logged in", () => {
-      // beforeEach(async ({ page }) => {
-      //   await page.getByRole("button", { name: "Login" }).click();
-      //   await page.getByLabel("Username:").fill("new User");
-      //   await page.getByLabel("Password:").fill("123");
-      //   await page.getByRole("button", { name: "Login" }).click();
-      //   await expect(page.getByText("Welcome, Junior!")).toBeVisible();
-      // });
+      beforeEach(async ({ page }) => {
+        // await page.getByRole("button", { name: "Login" }).click();
+        // await page.getByLabel("Username:").fill("new User");
+        // await page.getByLabel("Password:").fill("123");
+        // await page.getByRole("button", { name: "Login" }).click();
+        // await expect(page.getByText("Welcome, Junior!")).toBeVisible();
+
+        await page.goto("http://localhost:5173");
+      });
 
       test("a new blog can be created", async ({ page }) => {
-        await page.goto("http://localhost:5173");
+        // await page.getByRole("button", { name: "New Blog" }).click();
+        // await page.getByLabel("Title:").fill("New blog created");
+        // await page.getByLabel("Author:").fill("Junior");
+        // await page.getByLabel("Url:").fill("wwww");
+        // await page.getByRole("button", { name: "Create" }).click();
+        // await expect(page.getByText("Title: New blog created")).toBeVisible();
+      });
 
-        await page.getByRole("button", { name: "New Blog" }).click();
+      test("make sure the blog can be liked", async ({ page }) => {
+        await page.getByRole("button", { name: "View" }).click();
+        await page.getByRole("button", { name: "like" }).click();
 
-        await page.getByLabel("Title:").fill("New blog created");
-        await page.getByLabel("Author:").fill("Junior");
-        await page.getByLabel("Url:").fill("wwww");
-
-        await page.getByRole("button", { name: "Create" }).click();
-
-        await expect(page.getByText("Title: New blog created")).toBeVisible();
+        await expect(page.getByText("Likes: 0")).not.toBeVisible(); //CONTINUE
       });
     });
   });
